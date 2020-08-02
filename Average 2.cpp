af738fffbd673bc39df232d16549bef965f35809
omar mohamed <omarmeto268@gmail.com>
2020-08-02 14:39:43 +0200

    ++4

---
+++ Average 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(1);
+    cout << "MEDIA = " << (a*2 + b*3 + c*5) / 10 << endl;
+
+
+    return 0;
+}
