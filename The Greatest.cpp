b593fe095c1f6381f041df2e0515f8b3558d485d
omar mohamed <omarmeto268@gmail.com>
2020-08-05 14:21:35 +0200

    ++10

---
+++ The Greatest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << max(max(a, b), c) << " eh o maior" << endl;
+
+
+	return 0;
+
+
+
+}
